3e89d2c3af9bd688fa9071b4ef605b5f2ddf63d0
Jose Noronha <jnoronha@uolinc.com>
2023-06-26 15:41:40 -0300

    fix(pedidos): corrige obtencao de tempo total por pedido

--- data/pedidos.py
+++ data/pedidos.py
@@ -1,7 +1,11 @@
 import json
 from dataclasses import dataclass
+from statistics import mean
 
 from .sabores import intancia_sabor_por_nome
+import math
+
+tempos_pedidos = []
 
 
 @dataclass
@@ -15,17 +19,32 @@ class Pedido:
         self.numero_pedido = int(self.numero_pedido)
         self.items = [item.strip().lower() for item in self.items.split(',')]
         self.items = [intancia_sabor_por_nome(sabor) for sabor in self.items]
-        self.tempo_total_preparo = max([item.minutos_preparo for item in self.items])
+        self.tempo_total_preparo = self.__tempo_total_preparo()
+
+    def __tempo_total_preparo(self, capacidade_preparo_simultaneo: int = 3):
+        items_sorted_by_time = [_.minutos_preparo for _ in self.items]
+        items_sorted_by_time.sort(reverse=True)
+        tempo_total_preparo = 0
+        for i in range(0, len(self.items), capacidade_preparo_simultaneo):
+            tempo_total_preparo += max(
+                items_sorted_by_time[i : i + capacidade_preparo_simultaneo]
+            )
+        tempos_pedidos.append(tempo_total_preparo)
+        return tempo_total_preparo
 
 
 with open('data/pedidos.json', encoding='utf-8') as f_pedidos:
     pedidos = [Pedido(*pedido.values()) for pedido in json.load(f_pedidos)]
 
 
+print(f'tempo medio de preparo por pedido: {math.floor(mean(tempos_pedidos))} minutos')
+
+
 def get_pedido_por_numero(numero_pedido: int):
     for pedido in pedidos:
         if pedido.numero_pedido == numero_pedido:
             return pedido
+
 
 def get_pedido_maior_numero_items():
     pedido_mais_items = [pedidos[0]]
